7029776d2296931f0ce896b234db7d1672c7dd31
Stuart Quin <stuart.quin@gmail.com>
2018-08-15 18:20:56 +0100

    Add deployment step to CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 JS_DIRECTORY=build/static/js
 CSS_DIRECTORY=build/static/css
-APP=widget
 
 if [ ! -d "$JS_DIRECTORY" ]; then
     echo "JS DIR not found: $JS_DIRECTORY. Run yarn build"
@@ -23,4 +21,4 @@ cp ${CSS_DIRECTORY}/${APP}.*.css ${CSS_DIRECTORY}/${APP}.css
 aws s3 sync ${JS_DIRECTORY} s3://${AWS_S3_BUCKET}/client/${APP}/js --cache-control max-age=691200
 aws s3 sync ${CSS_DIRECTORY} s3://${AWS_S3_BUCKET}/client/${APP}/css --cache-control max-age=691200
 
-aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_ID} --paths "/client/widget/*"
+aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_ID} --paths "/client/${APP}/*"
