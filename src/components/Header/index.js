2f8a5f7eb2a02185a3480f2c26b6c4a3518c8a41
Fortunes Technology <fortunes.technology@gmail.com>
2018-08-23 16:14:37 +0800

    Update Login page and header icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-// import blackRideTologo from '../../assets/images/rideto-black.png'
-import whiteRideTologo from '../../assets/images/rideto-white.png'
-import greenRideToLogo from '../../assets/images/rideto_green.svg'
+import IconRideToLogo from '../../assets/icons/IconRideToLogo'
+
 import styles from './styles.scss'
 import classnames from 'classnames'
 
@@ -9,11 +8,9 @@ const Header = ({ dark, children }) => {
   return (
     <div className={classnames(styles.container, dark && styles.dark)}>
       <div className={styles.image}>
-        {dark ? (
-          <img src={whiteRideTologo} alt="RideTo logo" />
-        ) : (
-          <img src={greenRideToLogo} alt="RideTo logo" />
-        )}
+        <IconRideToLogo
+          className={classnames(styles.logoImage, dark && styles.darkLogo)}
+        />
       </div>
       <div className={styles.header}>{children}</div>
     </div>
