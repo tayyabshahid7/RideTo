14a91861850ef62bdc7c8e8852c9dd63576ab01f
Fortunes Technology <fortunes.technology@gmail.com>
2018-08-28 01:56:08 +0800

    Navbar and Calendar view update

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,48 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 // import Header from '../Header'
-// import UserMenu from '../UserMenu'
+import UserMenu from '../UserMenu'
 import classnames from 'classnames'
 import styles from './styles.scss'
 import IconRideToLogo from '../../assets/icons/IconRideToLogo'
 
 let NavigationBar = ({ history }) => {
   return (
-    // <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    //   <a class="navbar-brand" href="#">Top navbar</a>
-    //   <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    //     <span class="navbar-toggler-icon"></span>
-    //   </button>
-    //   <div class="navbar-collapse collapse" id="navbarCollapse">
-    //     <ul class="navbar-nav mr-auto">
-    //       <li className="nav-item active">
-    //         <NavLink
-    //           className={styles.navLink}
-    //           activeClassName={styles.activeNavLink}
-    //           exact
-    //           to="/">
-    //           Home
-    //         </NavLink>
-    //       </li>
-    //       <li className="nav-item">
-    //         <NavLink
-    //           className={styles.navLink}
-    //           activeClassName={styles.activeNavLink}
-    //           to="/orders">
-    //           Orders
-    //         </NavLink>
-    //       </li>
-    //       <li className="nav-item">
-    //         <NavLink
-    //           className={styles.navLink}
-    //           activeClassName={styles.activeNavLink}
-    //           to="/calendar">
-    //           Calendar
-    //         </NavLink>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </nav>
     <nav
       className={classnames(
         'navbar navbar-expand-md navbar-light bg-light fixed-top'
@@ -88,36 +53,11 @@ let NavigationBar = ({ history }) => {
             </NavLink>
           </li>
         </ul>
+        <form class={classnames('form-inline my-2 my-lg-0', styles.authMenu)}>
+          <UserMenu history={history} />
+        </form>
       </div>
     </nav>
-    // <Header>
-    //   <div className={styles.container}>
-    //     <div className={styles.navLinks}>
-    // <NavLink
-    //   className={styles.navLink}
-    //   activeClassName={styles.activeNavLink}
-    //   exact
-    //   to="/">
-    //   Home
-    // </NavLink>
-    // <NavLink
-    //   className={styles.navLink}
-    //   activeClassName={styles.activeNavLink}
-    //   to="/orders">
-    //   Orders
-    // </NavLink>
-    // <NavLink
-    //   className={styles.navLink}
-    //   activeClassName={styles.activeNavLink}
-    //   to="/calendar">
-    //   Calendar
-    // </NavLink>
-    //     </div>
-    //     <div className={styles.authMenu}>
-    //       <UserMenu history={history} />
-    //     </div>
-    //   </div>
-    // </Header>
   )
 }
 
