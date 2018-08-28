357fa8ae5360ef7ccf5c963bffd349d0edd5c1e6
Stuart Quin <stuart.quin@gmail.com>
2018-08-28 14:32:02 +0100

    WIP Customer detail form

--- src/pages/Customers/DetailFormContainer/index.js
+++ src/pages/Customers/DetailFormContainer/index.js
@@ -7,19 +7,37 @@ import commonStyles from 'pages/styles.scss'
 import CustomerDetailForm from 'pages/Customers/components/CustomerDetailForm'
 
 class DetailFormContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editable: null
+    }
+
+    this.handleChangeCustomer = this.handleChangeCustomer.bind(this)
+  }
+
   componentDidMount() {
     this.props.fetchCustomer(this.props.id)
   }
 
+  handleChangeCustomer(editable) {
+    this.setState({ editable })
+  }
+
   render() {
     const { customer } = this.props
+    const { editable } = this.state
 
     return (
       <div className={commonStyles.mainContent}>
         <h3>
           {customer.first_name} {customer.last_name}
         </h3>
-        <CustomerDetailForm customer={customer} />
+        <CustomerDetailForm
+          customer={editable || customer}
+          onChange={this.handleChangeCustomer}
+        />
       </div>
     )
   }
--- src/pages/Customers/components/CustomerDetailForm/index.js
+++ src/pages/Customers/components/CustomerDetailForm/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Row, Col } from 'reactstrap'
+import InputTextGroup from 'components/Forms/InputTextGroup'
 
-const CustomerDetailForm = ({ customer }) => {
+const handleChange = (customer, onChange) => ({ target }) => {
+  const { name, value } = target
+  onChange({ ...customer, [name]: value })
+}
+
+const CustomerDetailForm = ({ customer, onChange }) => {
+  const changeFn = handleChange(customer, onChange)
+
   return (
     <div>
       <Row>
-        <Col>{customer.phone}</Col>
+        <Col>
+          <InputTextGroup
+            name="phone"
+            value={customer.phone}
+            label="Phone"
+            className="form-group"
+            type="phone"
+            onChange={changeFn}
+          />
+        </Col>
         <Col>{customer.birthdate}</Col>
       </Row>
     </div>
