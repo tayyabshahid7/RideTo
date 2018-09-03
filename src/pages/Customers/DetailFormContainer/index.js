3915a18a4d0b87f84b430aefce1f501df21e5162
Stuart Quin <stuart.quin@gmail.com>
2018-09-03 10:32:52 +0100

    Show customer info in form heading

--- src/pages/Customers/DetailFormContainer/index.js
+++ src/pages/Customers/DetailFormContainer/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import moment from 'moment'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Col } from 'reactstrap'
 
 import styles from './DetailFormContainer.scss'
 import { actions, selectors } from 'store/customer'
+import { getCustomerType } from 'services/customer'
 import CustomerDetailForm from 'pages/Customers/components/CustomerDetailForm'
 import Loading from 'components/Loading'
+
+const getLastUpdated = date => {
+  return moment(date, 'YYYY-MM-DD').format('DD/MM/YYYY')
+}
 
 class DetailFormContainer extends React.Component {
   constructor(props) {
@@ -58,10 +64,20 @@ class DetailFormContainer extends React.Component {
     const isDisabled = !isChanged || isSaving
 
     return (
-      <Col className={styles.detailFormContainer}>
-        <h3>
-          {editable.first_name} {editable.last_name}
-        </h3>
+      <Col sm="6" className={styles.detailFormContainer}>
+        <div className={styles.heading}>
+          <h3>
+            {editable.first_name} {editable.last_name}
+          </h3>
+          <div className={styles.customerInfo}>
+            <div className={styles.source}>
+              {getCustomerType(editable.source)}
+            </div>
+            <div className={styles.updatedAt}>
+              Last updated: {getLastUpdated(editable.updated_at)}
+            </div>
+          </div>
+        </div>
         <Loading loading={isSaving}>
           <CustomerDetailForm
             customer={editable}
