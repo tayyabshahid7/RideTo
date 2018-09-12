d3f12bc8dfb9f18a1fbff6563ca6f9d2f6719984
Stuart Quin <stuart.quin@gmail.com>
2018-09-12 08:43:51 +0100

    Course slider arrow styles

--- src/components/RideTo/CourseSlider/index.js
+++ src/components/RideTo/CourseSlider/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import Slider from 'react-slick'
+import classnames from 'classnames'
 
 import { getStaticData } from 'services/page'
 import styles from './CourseSlider.scss'
+
+const NextArrow = ({ onClick }) => (
+  <div className={classnames(styles.arrow, styles.nextArrow)} onClick={onClick}>
+    {'>'}
+  </div>
+)
+
+const PrevArrow = ({ onClick }) => (
+  <div className={classnames(styles.arrow, styles.prevArrow)} onClick={onClick}>
+    {'<'}
+  </div>
+)
 
 const CourseSlider = () => {
   const settings = {
@@ -17,10 +30,14 @@ const CourseSlider = () => {
     className: styles.slider,
     dots: true,
     infinite: true,
+    arrows: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    centerMode: true
+    centerMode: true,
+    centerPadding: '64px',
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
   }
 
   const staticData = getStaticData('RIDETO_PAGE_HOME')
@@ -28,7 +45,14 @@ const CourseSlider = () => {
 
   return (
     <Slider {...settings}>
-      {slides.map(slide => <img src={slide.image} />)}
+      {slides.map(slide => (
+        <div>
+          <div
+            className={styles.slide}
+            style={{ backgroundImage: `url(${slide.image})` }}
+          />
+        </div>
+      ))}
     </Slider>
   )
 }
--- src/rideto.js
+++ src/rideto.js
@@ -7,8 +7,6 @@ import { setVersionInfo } from 'services/version'
 
 setVersionInfo()
 
-ReactDOM.render(<h1>RideTo.COM</h1>, document.getElementById('rideto-root'))
-
 ReactDOM.render(
   <CourseSlider />,
   document.getElementById('rideto-home-course-slider')
