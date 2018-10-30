9e095de887a1a64df6a45133cfa893a2347ccb1d
Carlos C <carlos@rideto.com>
2018-10-30 03:13:31 +0000

    HOTFIX removed lorem ipsum from FAQS description

--- src/components/RideTo/FaqsDetails/index.js
+++ src/components/RideTo/FaqsDetails/index.js
@@ -39,15 +39,7 @@ class FaqsDetails extends React.Component {
     return (
       <div className={styles.courseTypeDetails}>
         <div className={styles.title}>Frequently Asked Questions</div>
-        <div className={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+        <div className={styles.description} />
         <hr className={styles.divider} />
         <div className={styles.accordion}>
           {faqs.map((faq, index) => (
