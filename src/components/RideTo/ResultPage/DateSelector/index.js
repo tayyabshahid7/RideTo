5e9e039fccbfa2ca75ea595210340f2a9319abc3
Carlos C <carlos@rideto.com>
2018-10-30 03:55:34 +0000

    Removed loading component from date selector

--- src/components/RideTo/ResultPage/DateSelector/index.js
+++ src/components/RideTo/ResultPage/DateSelector/index.js
@@ -4,15 +4,13 @@ import classnames from 'classnames'
 import { SINGLE_DAY_IN_SECONDS } from 'common/constants'
 import styles from './styles.scss'
 import DateItem from './DateItem'
-import Loading from 'components/Loading'
 
 class DateSelector extends Component {
   constructor(props) {
     super(props)
     this.state = {
       startDate: new Date(),
-      daysCount: 12,
-      loadingDates: false
+      daysCount: 12
     }
     this.updateDimensions = this.updateDimensions.bind(this)
   }
@@ -42,7 +40,7 @@ class DateSelector extends Component {
     let today = moment().startOf('day')
     let newPrevDate = moment(date).startOf('day')
     if (newPrevDate < today) return
-    this.setState({ startDate: date, loadingDates: true })
+    this.setState({ startDate: date })
   }
 
   handleClickNext() {
@@ -50,11 +48,11 @@ class DateSelector extends Component {
     let date = new Date(
       startDate.getTime() + SINGLE_DAY_IN_SECONDS * 1000 * daysCount
     )
-    this.setState({ startDate: date, loadingDates: true })
+    this.setState({ startDate: date })
   }
 
   render() {
-    const { startDate, daysCount, loadingDates } = this.state
+    const { startDate, daysCount } = this.state
     const { date, handleSetDate, className } = this.props
     return (
       <div
@@ -63,23 +61,21 @@ class DateSelector extends Component {
         <div className={styles.prev} onClick={this.handleClickPrev.bind(this)}>
           <i className="fa fa-angle-left" aria-hidden="true" />
         </div>
-        <Loading loading={loadingDates}>
-          <div className={styles.dates}>
-            {Array.apply(null, { length: daysCount }).map((val, index) => (
-              <DateItem
-                date={
-                  new Date(
-                    startDate.getTime() + SINGLE_DAY_IN_SECONDS * 1000 * index
-                  )
-                }
-                activeDate={date}
-                showMonth={index === 0}
-                handleSetDate={handleSetDate}
-                key={index}
-              />
-            ))}
-          </div>
-        </Loading>
+        <div className={styles.dates}>
+          {Array.apply(null, { length: daysCount }).map((val, index) => (
+            <DateItem
+              date={
+                new Date(
+                  startDate.getTime() + SINGLE_DAY_IN_SECONDS * 1000 * index
+                )
+              }
+              activeDate={date}
+              showMonth={index === 0}
+              handleSetDate={handleSetDate}
+              key={index}
+            />
+          ))}
+        </div>
         <div className={styles.next} onClick={this.handleClickNext.bind(this)}>
           <i className="fa fa-angle-right" aria-hidden="true" />
         </div>
