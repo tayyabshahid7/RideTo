17d6881f2f77b4d21ac80624b87065077d40ea1a
Carlos C <carlos@rideto.com>
2018-11-16 19:51:09 +0000

    Working on auto selecting date when default date not available

--- src/components/RideTo/AvailabilityCalendar/index.js
+++ src/components/RideTo/AvailabilityCalendar/index.js
@@ -4,8 +4,43 @@ import CalendarHeader from './CalendarHeader'
 import styles from './index.scss'
 import CalendarContent from './CalendarContent'
 import CalendarTime from './CalendarTime'
+import moment from 'moment'
 
 class AvailabilityCalendar extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    const {
+      calendar: { selectedDate },
+      days,
+      isInstantBook
+    } = this.props
+    if (selectedDate && this.isSelectedDateDisabled(selectedDate, days)) {
+      const firstAvailableDate = this.getFirstAvailableDate(days, isInstantBook)
+      this.props.handleDateSelect(firstAvailableDate)
+    }
+  }
+
+  getFirstAvailableDate(days, isInstantBook) {
+    for (let i = 0; i < days.length; i++) {
+      if (!days[i].disabled) {
+        if (!isInstantBook) {
+          return moment(days[i].date).format('YYYY-MM-DD')
+        } else if (days.courses.length > 0) {
+          return moment(days[i].date).format('YYYY-MM-DD')
+        }
+      }
+    }
+  }
+
+  isSelectedDateDisabled(date, days) {
+    const selectedDate = moment(date)
+    for (let i = 0; i < days.length; i++) {
+      if (days[i].disabled && days[i].date.getDate() === selectedDate.date()) {
+        return true
+      }
+    }
+    return false
+  }
+
   render() {
     let {
       days,
