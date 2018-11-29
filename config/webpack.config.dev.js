c79d4a119593c63293bf8a47fbde89a4f314d4a1
James Coventry <jamescoventryy@gmail.com>
2018-11-29 11:08:37 +0000

    PRODEV-775. Load bike-test components dynamically

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -11,8 +11,8 @@ const getClientEnvironment = require('./env')
 const paths = require('./paths')
 
 // Webpack uses `publicPath` to determine where the app is being served from.
-// In development, we always serve from the root. This makes config easier.
-const publicPath = '/'
+// In development, we set localhost:3000 to allow dynamic imports
+const publicPath = 'http://localhost:3000/'
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
