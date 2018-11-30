90d27af715b574499a6ba1bd762f0f028611b278
Carlos C <carlos@rideto.com>
2018-11-30 11:59:10 +0000

    PRODEV-775. Changed gitlab CI. Deploy script to put everything on 'clients/*' directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,40 +17,16 @@ if [ ! -d "$BUILD_CSS_DIR" ]; then
     exit 1
 fi
 
-DEPLOY_APPS=(${APPS//,/ })
-CDN_PATHS=""
+echo ""
+echo "  Deploying..."
+echo ""
+echo "  Cloudfront ID: $AWS_CLOUDFRONT_ID"
+echo "  Bucket: $AWS_S3_BUCKET"
+echo ""
 
-
-
-for i in "${!DEPLOY_APPS[@]}"
-do
-    APP="${DEPLOY_APPS[i]}"
-    APP_JS_DIR=${BUILD_JS_DIR}/${APP}
-    APP_CSS_DIR=${BUILD_JS_DIR}/${APP}
-
-    echo "Deploying $APP"
-    echo ""
-    echo "  Cloudfront ID: $AWS_CLOUDFRONT_ID"
-    echo "  Bucket: $AWS_S3_BUCKET"
-    echo ""
-
-    mkdir -p $APP_JS_DIR
-    mkdir -p $APP_CSS_DIR
-
-    # mv ${BUILD_JS_DIR}/${APP}.*.js.map ${APP_JS_DIR}/${APP}.js.map
-    mv ${BUILD_JS_DIR}/${APP}.js ${APP_JS_DIR}/
-    mv ${BUILD_JS_DIR}/${APP}.*.js ${APP_JS_DIR}/
-    mv ${BUILD_CSS_DIR}/*.css ${APP_CSS_DIR}/
-
-    if [ $? -eq 0 ]
-    then
-        aws s3 sync ${APP_JS_DIR} s3://${AWS_S3_BUCKET}/client/${APP}/js --cache-control max-age=691200
-        aws s3 sync ${APP_CSS_DIR} s3://${AWS_S3_BUCKET}/client/${APP}/css --cache-control max-age=691200
-        CDN_PATHS="$CDN_PATHS /client/${APP}/*"
-    else
-        echo "Could not find built assets for $APP, ensure yarn build"
-    fi
-done
+aws s3 sync ${BUILD_JS_DIR} s3://${AWS_S3_BUCKET}/client/js --cache-control max-age=691200
+aws s3 sync ${BUILD_CSS_DIR} s3://${AWS_S3_BUCKET}/client/css --cache-control max-age=691200
+CDN_PATHS="$CDN_PATHS /client/*"
 
 if [ "${CDN_PATHS}" ]; then
     echo "Invalidate Cloudfront: $CDN_PATHS"
