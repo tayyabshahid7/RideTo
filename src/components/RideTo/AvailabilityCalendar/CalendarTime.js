3aff22b04aadf5889a1d35e35ad873dac3e361b5
James Coventry <jamesc@rideto.com>
2018-12-10 10:31:14 +0000

    PRODEV-803. Change CalendarTime to smart component

--- src/components/RideTo/AvailabilityCalendar/CalendarTime.js
+++ src/components/RideTo/AvailabilityCalendar/CalendarTime.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classnames from 'classnames'
 import styles from './CalendarTime.scss'
 
-const CalendarTime = ({ calendar, courses, handleTimeSelect }) => {
-  return (
-    <div className={styles.container}>
-      {courses.map(
-        (course, index) =>
-          course.spaces > course.order_count && (
-            <button
-              key={index}
-              className={classnames(
-                styles.btn,
-                calendar.selectedCourse &&
-                  calendar.selectedCourse.id === course.id &&
-                  styles.activeBtn
-              )}
-              onClick={() => handleTimeSelect(course)}>
-              {course.time.substring(0, 5)}
-            </button>
-          )
-      )}
-    </div>
-  )
+class CalendarTime extends Component {
+  render() {
+    const { calendar, courses, handleTimeSelect } = this.props
+
+    return (
+      <div className={styles.container}>
+        {courses.map(
+          (course, index) =>
+            course.spaces > course.order_count && (
+              <button
+                key={index}
+                className={classnames(
+                  styles.btn,
+                  calendar.selectedCourse &&
+                    calendar.selectedCourse.id === course.id &&
+                    styles.activeBtn
+                )}
+                onClick={() => handleTimeSelect(course)}>
+                {course.time.substring(0, 5)}
+              </button>
+            )
+        )}
+      </div>
+    )
+  }
 }
 
 export default CalendarTime
