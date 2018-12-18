06106d1284ae93f90a6a4a30b7e8f0a32b865794
James Coventry <jamesc@rideto.com>
2018-12-18 17:21:36 +0000

    PRODEV-845. Remove annoying apostrophe from manual info message

--- src/components/RideTo/ResultPage/CourseDetailPanel/CourseAvailabilityComponent.js
+++ src/components/RideTo/ResultPage/CourseDetailPanel/CourseAvailabilityComponent.js
@@ -198,7 +198,7 @@ class CourseAvailabilityComponent extends React.Component {
     const fullText = <span className={styles.full}> - Fully Booked</span>
     const manualText = (
       <span className={styles.manualInfo}>
-        Please note: If you haven’t ridden a manual (geared) bike before and
+        Please note: If you haven't ridden a manual (geared) bike before and
         expect to complete the CBT training in one day, you may find you need
         additional training to complete your CBT. If your not sure, call us on
         02036039652 to talk to a member of our friendly team.
