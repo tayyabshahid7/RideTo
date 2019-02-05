8bd8c559d4416fffb14e2e18b29b3f058a572b96
James Coventry <jamesc@rideto.com>
2019-02-05 15:52:42 +0000

    PRODEV-932. Add start times to DAS date pickers

--- src/components/RideTo/CheckoutPage/OrderSummary/index.js
+++ src/components/RideTo/CheckoutPage/OrderSummary/index.js
@@ -79,7 +79,9 @@ class OrderSummary extends Component {
                       'Full Licence ',
                       ''
                     ),
-                    moment(training.requested_date).format('ddd D, MMMM')
+                    `${training.requested_time.slice(0, -3)} ${moment(
+                      training.requested_date
+                    ).format('ddd D, MMMM')}`
                   )}
                 </div>
               )
