184f9b7f30119c202679f26fa13cb15d2744b0c1
James Coventry <jamesc@rideto.com>
2019-02-26 16:04:34 +0000

    PRODEV-1098. Make whole dashboard checkbox clickable

--- src/components/RideTo/Account/DashboardChecklist/index.js
+++ src/components/RideTo/Account/DashboardChecklist/index.js
@@ -10,23 +10,20 @@ const DashboardChecklist = ({ items }) => {
       <h4>My Checklist</h4>
 
       {items.map((item, index) => (
-        <div
+        <a
           key={index}
-          className={classnames(styles.item, item.checked && styles.checked)}>
+          className={classnames(styles.item, item.checked && styles.checked)}
+          href={item.href}
+          target="_blank"
+          rel="noopener noreferrer">
           <Checkbox
             extraClass={styles.checkbox}
             size="large"
             checked={item.checked}
             name={item.text}>
-            <a
-              href={item.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.checkLinks}>
-              {item.text}
-            </a>
+            <span className={styles.checkLinks}>{item.text}</span>
           </Checkbox>
-        </div>
+        </a>
       ))}
     </div>
   )
