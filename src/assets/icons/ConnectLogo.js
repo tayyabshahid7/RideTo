7ca4af8342b0c995ae44b4988cf1691731a1526c
James Coventry <jamesc@rideto.com>
2019-03-26 13:25:31 +0000

    PRODEV-1081. Add logos

---
+++ src/assets/icons/ConnectLogo.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function ConnectLogo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40">
+      <circle cx="20" cy="20" r="20" fill="#4259a8" />
+      <path
+        fill="none"
+        stroke="#fff"
+        strokeWidth="1.7972445000000001"
+        strokeMiterlimit="10"
+        d="M25.079 11.754l-13.76 8.106 9.927 10.549"
+      />
+      <circle
+        cx="5.914"
+        cy="22.082"
+        r="4.188"
+        transform="rotate(-14.68)"
+        fill="#4bbfa5"
+        stroke="#fff"
+        strokeWidth="1.348"
+        strokeMiterlimit="10"
+      />
+      <circle
+        cx="19.901"
+        cy="19.261"
+        r="5.177"
+        transform="rotate(-18.96)"
+        fill="#54b5c8"
+        stroke="#fff"
+        strokeWidth="1.348"
+        strokeMiterlimit="10"
+      />
+      <circle
+        cx="10.101"
+        cy="35.525"
+        r="3.293"
+        transform="rotate(-18.96)"
+        fill="#c2c2c1"
+        stroke="#fff"
+        strokeWidth="1.348"
+        strokeMiterlimit="10"
+      />
+    </svg>
+  )
+}
+
+export default ConnectLogo
