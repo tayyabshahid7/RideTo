eaa65174b28de71b72668a977d4258b71cc3a484
James Coventry <jamesc@rideto.com>
2019-03-27 11:53:41 +0000

    PRODEV-1081. Update login page

--- src/assets/icons/ConnectLogoFull.js
+++ src/assets/icons/ConnectLogoFull.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function ConnectLogoFull() {
+function ConnectLogoFull(props) {
   return (
     <svg
+      {...props}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1466.102 337.18"
       id="Layer_1">
