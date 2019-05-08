c008a75b3567e432f7adf1baff048460550408da
James Coventry <jamesc@rideto.com>
2019-05-08 14:43:10 +0100

    PRODEV-1269. Add SMS fields

--- src/pages/Account/SMSSettings/SMSSettings.js
+++ src/pages/Account/SMSSettings/SMSSettings.js
@@ -2,7 +2,7 @@ import React, { Fragment, Component } from 'react'
 import styles from './styles.scss'
 import { Row, Col, Modal, ModalBody } from 'reactstrap'
 import classnames from 'classnames'
-import { Button } from 'components/ConnectForm'
+import { Button, ConnectTextArea } from 'components/ConnectForm'
 import CheckoutForm from 'components/Calendar/OrdersPanel/AddOrderItem/CheckoutForm'
 import { injectStripe } from 'react-stripe-elements'
 import Loading from 'components/Loading'
@@ -11,19 +11,36 @@ class SMSSettings extends Component {
   constructor(props) {
     super(props)
 
+    this.defaultSettings = {
+      cbt_booking_confirmation: '',
+      full_licence_booking_confirmation: '',
+      post_cbt_feedback: '',
+      ...this.props.settings
+    }
+
     this.state = {
       cardName: '',
       selectedValue: null,
       cardNumberComplete: false,
       cardDateComplete: false,
       cardCVCComplete: false,
-      cardPostCodeComplete: false
+      cardPostCodeComplete: false,
+      settings: this.defaultSettings,
+      cbt_booking_confirmationEditable: false,
+      full_licence_booking_confirmationEditable: false,
+      post_cbt_feedbackEditable: false
     }
+
+    this.cbt_booking_confirmationEl = React.createRef()
+    this.full_licence_booking_confirmationEl = React.createRef()
+    this.post_cbt_feedbackEl = React.createRef()
 
     this.handleSave = this.handleSave.bind(this)
     this.handleCancel = this.handleCancel.bind(this)
     this.handleCardNameChange = this.handleCardNameChange.bind(this)
     this.handleStripeElementChange = this.handleStripeElementChange.bind(this)
+    this.handleEditClick = this.handleEditClick.bind(this)
+    this.handleSaveClick = this.handleSaveClick.bind(this)
   }
 
   componentDidMount() {
@@ -40,6 +57,12 @@ class SMSSettings extends Component {
 
   handleStripeElementChange(el, name) {
     this.setState({ [`card${name}Complete`]: !el.empty && el.complete })
+  }
+
+  handleChangeRawEvent(event) {
+    const { name, value } = event.target
+    const { settings } = this.state
+    this.setState({ settings: { ...settings, [name]: value } })
   }
 
   handleCancel() {
@@ -75,15 +98,49 @@ class SMSSettings extends Component {
     }
   }
 
+  handleEditClick(el) {
+    this.setState(
+      {
+        [`${el}Editable`]: true
+      },
+      () => {
+        const {
+          current: {
+            el: { current }
+          }
+        } = this[`${el}El`]
+
+        current.focus()
+      }
+    )
+  }
+
+  handleSaveClick(el) {
+    const { updateWidgetSettings } = this.props
+    const { settings } = this.state
+    updateWidgetSettings(settings)
+    this.setState({
+      [`${el}Editable`]: false
+    })
+  }
+
   render() {
-    const { credit_unit, loading } = this.props
+    const { credit_unit, smsLoading, saving } = this.props
     const {
       selectedValue,
       cardName,
       cardNumberComplete,
       cardDateComplete,
       cardCVCComplete,
-      cardPostCodeComplete
+      cardPostCodeComplete,
+      settings: {
+        cbt_booking_confirmation,
+        full_licence_booking_confirmation,
+        post_cbt_feedback
+      },
+      cbt_booking_confirmationEditable,
+      full_licence_booking_confirmationEditable,
+      post_cbt_feedbackEditable
     } = this.state
 
     return (
@@ -128,11 +185,132 @@ class SMSSettings extends Component {
                 </div>
               </div>
             </div>
+            <Loading loading={saving}>
+              <div className={classnames(styles.box, styles.boxVertical)}>
+                <h3 className={styles.title}>SMS templates</h3>
+                <p className={styles.intro}>
+                  Write the copy that you wish to display in your SMS
+                  communication to customers
+                </p>
+                <div>
+                  <Row>
+                    <Col>
+                      <ConnectTextArea
+                        ref={this.cbt_booking_confirmationEl}
+                        noBorder
+                        autoHeight
+                        name="cbt_booking_confirmation"
+                        value={cbt_booking_confirmation}
+                        label="CBT booking confirmation"
+                        type="textarea"
+                        onChange={this.handleChangeRawEvent.bind(this)}
+                        disabled={!cbt_booking_confirmationEditable}
+                      />
+                    </Col>
+                    <Col xs="2">
+                      <div className={styles.editButton}>
+                        <Button
+                          color="link"
+                          onClick={() => {
+                            this.handleEditClick('cbt_booking_confirmation')
+                          }}>
+                          Edit
+                        </Button>
+                        {cbt_booking_confirmationEditable && (
+                          <Button
+                            color="link"
+                            onClick={() => {
+                              this.handleSaveClick('cbt_booking_confirmation')
+                            }}>
+                            Save
+                          </Button>
+                        )}
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <ConnectTextArea
+                        ref={this.full_licence_booking_confirmationEl}
+                        noBorder
+                        autoHeight
+                        name="full_licence_booking_confirmation"
+                        value={full_licence_booking_confirmation}
+                        label="Full licence booking confirmation"
+                        type="textarea"
+                        onChange={this.handleChangeRawEvent.bind(this)}
+                        disabled={!full_licence_booking_confirmationEditable}
+                      />
+                    </Col>
+                    <Col xs="2">
+                      <div className={styles.editButton}>
+                        <Button
+                          color="link"
+                          onClick={() => {
+                            this.handleEditClick(
+                              'full_licence_booking_confirmation'
+                            )
+                          }}>
+                          Edit
+                        </Button>
+                        {full_licence_booking_confirmationEditable && (
+                          <Button
+                            color="link"
+                            onClick={() => {
+                              this.handleSaveClick(
+                                'full_licence_booking_confirmation'
+                              )
+                            }}>
+                            Save
+                          </Button>
+                        )}
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <ConnectTextArea
+                        ref={this.post_cbt_feedbackEl}
+                        noBorder
+                        autoHeight
+                        name="post_cbt_feedback"
+                        value={post_cbt_feedback}
+                        label="Post CBT feedback"
+                        type="textarea"
+                        onChange={this.handleChangeRawEvent.bind(this)}
+                        disabled={!post_cbt_feedbackEditable}
+                      />
+                    </Col>
+                    <Col xs="2">
+                      <div className={styles.editButton}>
+                        <Button
+                          color="link"
+                          onClick={() => {
+                            this.handleEditClick('post_cbt_feedback')
+                          }}>
+                          Edit
+                        </Button>
+                        {post_cbt_feedbackEditable && (
+                          <Button
+                            color="link"
+                            onClick={() => {
+                              this.handleSaveClick('post_cbt_feedback')
+                            }}>
+                            Save
+                          </Button>
+                        )}
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+            </Loading>
           </Col>
         </Row>
+
         <Modal isOpen={!!selectedValue} fade={false}>
           <ModalBody>
-            <Loading loading={loading}>
+            <Loading loading={smsLoading}>
               <form onSubmit={this.handleSave}>
                 <CheckoutForm
                   price={selectedValue * 100}
--- src/pages/Account/SMSSettings/index.js
+++ src/pages/Account/SMSSettings/index.js
@@ -1,11 +1,36 @@
+import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import SMSSettings from './SMSSettings'
 import { fetchCredits, purchaseCredits } from 'store/sms'
+import { fetchWidgetSettings, updateWidgetSettings } from 'store/settings'
+
+class SMSSettingsContainer extends Component {
+  componentDidMount() {
+    const { fetchWidgetSettings, settings } = this.props
+    if (!settings) {
+      fetchWidgetSettings()
+    }
+  }
+
+  render() {
+    const { loading, settings } = this.props
+    if (loading || !settings) {
+      return <div>Loading...</div>
+    }
+    return <SMSSettings {...this.props} />
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    loading: state.sms.loading,
+    schoolId: state.auth.schoolId,
+    loading: state.settings.widget.loading,
+    saving: state.settings.widget.saving,
+    settings: state.settings.widget.settings,
+    error: state.settings.widget.error,
+
+    smsLoading: state.sms.loading,
     credit_unit: state.sms.credit_unit,
     sms_credit: state.sms.sms_credit
   }
@@ -15,7 +40,9 @@ const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
       fetchCredits,
-      purchaseCredits
+      purchaseCredits,
+      fetchWidgetSettings,
+      updateWidgetSettings
     },
     dispatch
   )
@@ -23,4 +50,4 @@ const mapDispatchToProps = dispatch =>
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SMSSettings)
+)(SMSSettingsContainer)
