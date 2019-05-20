95d886b241e3804a236eda0f0246c8fb0aa0456a
James Coventry <jamesc@rideto.com>
2019-05-20 13:53:04 +0100

    PRODEV-1174. Improve routing

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -11,7 +11,7 @@ const ProtectedRoute = ({ component: Component, ...rest }) => (
       ) : (
         <Redirect
           to={{
-            pathname: '/login',
+            pathname: '/home',
             state: { from: props.location }
           }}
         />
