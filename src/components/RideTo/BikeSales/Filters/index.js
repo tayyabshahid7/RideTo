95d1f0af5150f9fd11fcdc17a17bb8e94cf3033c
James Coventry <jamesc@rideto.com>
2019-05-22 14:07:57 +0100

    PRODEV-1282. Get checkbox filters working nicely

--- src/components/RideTo/BikeSales/Filters/index.js
+++ src/components/RideTo/BikeSales/Filters/index.js
@@ -12,53 +12,53 @@ import {
 class Filters extends Component {
   constructor(props) {
     super(props)
+
+    this.handleCheckboxClick = this.handleCheckboxClick.bind(this)
+  }
+
+  handleCheckboxClick(
+    {
+      target: { checked }
+    },
+    name,
+    value
+  ) {
+    const { updateFilters } = this.props
+
+    updateFilters(checked, name, value)
   }
 
   render() {
+    const { options } = this.props
+
     return (
       <div>
         <div>Filterszz</div>
         <Accordion allowMultipleExpanded allowZeroExpanded>
-          <Item className={styles.accordionItem}>
-            <Heading>
-              <Button className={styles.accordionButton}>
-                Brand <Selected />
-              </Button>
-            </Heading>
-            <Panel>asdf</Panel>
-          </Item>
-          <Item className={styles.accordionItem}>
-            <Heading>
-              <Button className={styles.accordionButton}>
-                Engine <Selected />
-              </Button>
-            </Heading>
-            <Panel>asdf</Panel>
-          </Item>
-          <Item className={styles.accordionItem}>
-            <Heading>
-              <Button className={styles.accordionButton}>
-                Style <Selected />
-              </Button>
-            </Heading>
-            <Panel>asdf</Panel>
-          </Item>
-          <Item className={styles.accordionItem}>
-            <Heading>
-              <Button className={styles.accordionButton}>
-                Budget <Selected />
-              </Button>
-            </Heading>
-            <Panel>asdf</Panel>
-          </Item>
-          <Item className={styles.accordionItem}>
-            <Heading>
-              <Button className={styles.accordionButton}>
-                Licence <Selected />
-              </Button>
-            </Heading>
-            <Panel>asdf</Panel>
-          </Item>
+          {options.map(({ name, values }) => (
+            <Item key={name} className={styles.accordionItem}>
+              <Heading>
+                <Button className={styles.accordionButton}>
+                  {name} <Selected />
+                </Button>
+              </Heading>
+              <Panel className={styles.accordionPanel}>
+                {values.map(value => (
+                  <div key={value}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        onClick={event => {
+                          this.handleCheckboxClick(event, name, value)
+                        }}
+                      />{' '}
+                      {value}
+                    </label>
+                  </div>
+                ))}
+              </Panel>
+            </Item>
+          ))}
         </Accordion>
       </div>
     )
--- src/components/RideTo/BikeSales/index.js
+++ src/components/RideTo/BikeSales/index.js
@@ -1,76 +1,156 @@
 import React, { Component } from 'react'
 import SlidingPane from 'react-sliding-pane'
+import Modal from 'react-modal'
 import styles from './styles.scss'
 import BikeSummary from './BikeSummary'
 import Filters from './Filters'
 import 'react-sliding-pane/dist/react-sliding-pane.css'
+import { isEqual } from 'lodash'
 
 const DUMMY_DATA = [
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda MSX 125',
+    name: 'Honda MSADV 125',
     price: 420000,
     desc: 'Blah blah blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      engine: '125cc',
+      licence: 'CBT',
+      brand: 'Honda',
+      style: 'Adventure'
+    }
   },
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda CBR125',
+    name: 'Honda CBR125 Classy',
     price: 344900,
     desc: 'Blah lorem blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      engine: '125cc',
+      licence: 'CBT',
+      brand: 'Honda',
+      style: 'Classic'
+    }
   },
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda MSX 125',
+    name: 'Ducati MSX 2000cc ADV',
     price: 420000,
     desc: 'Blah blah blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      licence: 'A2 Licence',
+      engine: '2000cc',
+      brand: 'Ducati',
+      style: 'Adventure'
+    }
   },
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda CBR125',
+    name: 'Ducati CBR125 Super Moto',
     price: 344900,
     desc: 'Blah lorem blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      engine: '125cc',
+      licence: 'CBT',
+      brand: 'Ducati',
+      style: 'Super Moto'
+    }
   },
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda MSX 125',
+    name: 'Honda MSX 300 Adventure',
     price: 420000,
     desc: 'Blah blah blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      licence: 'A1 Licence',
+      engine: '300cc',
+      brand: 'Honda',
+      style: 'Adventure'
+    }
   },
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda CBR125',
+    name: 'BMW CBR125 Classic',
     price: 344900,
     desc: 'Blah lorem blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      engine: '125cc',
+      licence: 'CBT',
+      brand: 'BMW',
+      style: 'Classic'
+    }
   },
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda MSX 125',
+    name: 'Honda MSX 125 Classico',
     price: 420000,
     desc: 'Blah blah blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      engine: '125cc',
+      licence: 'CBT',
+      brand: 'Honda',
+      style: 'Classic'
+    }
   },
   {
     image: 'https://via.placeholder.com/330x200',
-    name: 'Honda CBR125',
+    name: 'Honda CBR125 Adventure',
     price: 344900,
     desc: 'Blah lorem blah blah',
     bookLink: 'https://www.google.com/',
-    reviewLink: 'https://www.google.com/'
+    reviewLink: 'https://www.google.com/',
+    categories: {
+      engine: '125cc',
+      licence: 'CBT',
+      brand: 'Honda',
+      style: 'Adventure'
+    }
   }
 ]
+
+const FILTERS = [
+  { name: 'Brand', values: ['Honda', 'BMW', 'Susuki', 'Ducati'] },
+  {
+    name: 'Engine',
+    values: ['50cc', '125cc', '200cc', '300cc', '400cc', '2000cc']
+  },
+  { name: 'Style', values: ['Adventure', 'Classic', 'Scooter', 'Super Moto'] },
+  { name: 'Budget', values: [] },
+  { name: 'Licence', values: ['CBT', 'A1 Licence', 'A2 Licence', 'A Licence'] }
+]
+
+function reduceFilters(filters) {
+  return filters.reduce((obj, option) => {
+    const name = option.name.toLowerCase()
+
+    obj[name] = option.values
+      .filter(value => value.active)
+      .reduce((arr, value) => {
+        arr.push(value.name)
+        return arr
+      }, [])
+
+    if (obj[name].length === 0) {
+      obj[name] = 'All'
+    }
+
+    return obj
+  }, {})
+}
 
 class BikeSales extends Component {
   constructor(props) {
@@ -78,11 +158,68 @@ class BikeSales extends Component {
 
     this.state = {
       filtersOpen: false,
-      sortOpen: false
+      sortOpen: false,
+      filters: FILTERS.map(option => ({
+        ...option,
+        values: option.values.map(value => ({
+          name: value,
+          active: false
+        }))
+      })),
+      bikes: DUMMY_DATA
     }
 
+    this.updateFilters = this.updateFilters.bind(this)
     this.handleFiltersButtonClick = this.handleFiltersButtonClick.bind(this)
     this.handleSortButtonClick = this.handleSortButtonClick.bind(this)
+  }
+
+  componentDidMount() {
+    Modal.setAppElement(document.getElementById('bike-sales-root'))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!isEqual(this.state.filters, prevState.filters)) {
+      const reducedFilters = reduceFilters(this.state.filters)
+
+      this.setState({
+        bikes: DUMMY_DATA.filter(bike => {
+          return Object.entries(bike.categories).every(([name, value]) => {
+            if (
+              reducedFilters[name] !== 'All' &&
+              !reducedFilters[name].includes(value)
+            ) {
+              return false
+            }
+            return true
+          })
+        })
+      })
+    }
+  }
+
+  updateFilters(active, name, valueToSet) {
+    this.setState({
+      filters: this.state.filters.map(option => {
+        if (option.name === name) {
+          return {
+            ...option,
+            values: option.values.map(value => {
+              if (value.name === valueToSet) {
+                return {
+                  ...value,
+                  active
+                }
+              }
+
+              return value
+            })
+          }
+        }
+
+        return option
+      })
+    })
   }
 
   handleFiltersButtonClick() {
@@ -100,7 +237,7 @@ class BikeSales extends Component {
   }
 
   render() {
-    const { filtersOpen, sortOpen } = this.state
+    const { filtersOpen, sortOpen, bikes } = this.state
 
     return (
       <div className={styles.page}>
@@ -123,14 +260,14 @@ class BikeSales extends Component {
               </button>
             </div>
             <div className={styles.results}>
-              Results: {DUMMY_DATA.length} bikes found
+              Results: {bikes.length} bikes found
             </div>
           </div>
         </div>
         <div className={styles.listing}>
           <div className={styles.container}>
             <div className={styles.row}>
-              {DUMMY_DATA.map((bike, i) => (
+              {bikes.map((bike, i) => (
                 <div className={styles.item} key={i}>
                   <BikeSummary bike={bike} />
                 </div>
@@ -139,13 +276,15 @@ class BikeSales extends Component {
           </div>
         </div>
         <SlidingPane
+          width="60%"
           className={styles.panel}
           isOpen={filtersOpen}
           onRequestClose={this.handleFiltersButtonClick}
           from="left">
-          <Filters />
+          <Filters options={FILTERS} updateFilters={this.updateFilters} />
         </SlidingPane>
         <SlidingPane
+          width="60%"
           className={styles.panel}
           isOpen={sortOpen}
           onRequestClose={this.handleSortButtonClick}
