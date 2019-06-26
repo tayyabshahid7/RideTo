e77f93eddba064aa24d55c5771be1d49fad6851f
James Coventry <jamesc@rideto.com>
2019-06-26 10:14:24 +0100

    PRODEV-1127. Move promocode into method

--- src/components/RideTo/CheckoutPage/index.js
+++ src/components/RideTo/CheckoutPage/index.js
@@ -47,6 +47,7 @@ class CheckoutPageContainer extends Component {
     this.handleSetDate = this.handleSetDate.bind(this)
     this.handeUpdateOption = this.handeUpdateOption.bind(this)
     this.handlePOMToggleClick = this.handlePOMToggleClick.bind(this)
+    this.showPromoNotification = this.showPromoNotification.bind(this)
 
     this.POM = createPOM()
   }
@@ -67,40 +68,22 @@ class CheckoutPageContainer extends Component {
       newCheckoutData.addons = [...newCheckoutData.addons, this.POM]
     }
 
-    this.setState(
-      {
-        checkoutData: newCheckoutData,
-        hasPOM: true
-      },
-      () => {
-        toast.dismiss('remove')
-        toast('Peace of mind policy added!', {
-          toastId: 'add',
-          className: styles.toastAdd
-        })
-      }
-    )
+    this.setState({
+      checkoutData: newCheckoutData,
+      hasPOM: true
+    })
   }
 
   handleRemovePOM() {
     const { checkoutData } = this.state
 
-    this.setState(
-      {
-        checkoutData: {
-          ...checkoutData,
-          addons: checkoutData.addons.filter(addon => addon.name !== POM_NAME)
-        },
-        hasPOM: false
+    this.setState({
+      checkoutData: {
+        ...checkoutData,
+        addons: checkoutData.addons.filter(addon => addon.name !== POM_NAME)
       },
-      () => {
-        toast.dismiss('add')
-        toast('Peace of mind policy removed.', {
-          toastId: 'remove',
-          className: styles.toastRemove
-        })
-      }
-    )
+      hasPOM: false
+    })
   }
 
   handlePOMToggleClick() {
@@ -111,6 +94,13 @@ class CheckoutPageContainer extends Component {
     } else {
       this.handleAddPOM()
     }
+  }
+
+  showPromoNotification() {
+    toast('Promo code applied!', {
+      toastId: 'add',
+      className: styles.toastAdd
+    })
   }
 
   render() {
@@ -141,6 +131,7 @@ class CheckoutPageContainer extends Component {
               trainings={trainings}
               handlePOMToggleClick={this.handlePOMToggleClick}
               hasPOM={hasPOM}
+              showPromoNotification={this.showPromoNotification}
             />
           </Elements>
         </StripeProvider>
