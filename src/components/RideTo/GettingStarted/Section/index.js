eac227af9128a6a695fd1d45788f0543341cb4b9
James Coventry <jamesc@rideto.com>
2019-07-11 14:09:29 +0100

    PRODEV-1384. Wrap waypoint

--- src/components/RideTo/GettingStarted/Section/index.js
+++ src/components/RideTo/GettingStarted/Section/index.js
@@ -18,39 +18,43 @@ const Section = React.forwardRef(function(
 
   return (
     <div className={styles.section} ref={ref}>
-      <Waypoint onEnter={handleEnter} />
-      <div className={styles.sectionInner}>
-        <div className={styles.sectionImageWrap}>
-          <img className={styles.sectionImage} src={img} alt={name} />
+      <Waypoint onEnter={handleEnter}>
+        <div className={styles.sectionInner}>
+          <div className={styles.sectionImageWrap}>
+            <img className={styles.sectionImage} src={img} alt={name} />
+          </div>
+          <div className={styles.sectionContent}>
+            <h3 className={styles.sectionName}>{name}</h3>
+            <h2 className={styles.sectionTitle}>{title}</h2>
+            {!!text && <p>{text}</p>}
+            {!!Object.keys(faqs).length && (
+              <CourseTypeDetails
+                courseType={{ details: faqs }}
+                minimal
+                spacedOut
+              />
+            )}
+            {!!links.length && (
+              <div className={styles.sectionButtons}>
+                {links.map(({ url, text, alt, external }, i) => (
+                  <RideToButton
+                    className={styles.sectionButton}
+                    key={i}
+                    href={url}
+                    alt={alt}
+                    target={external ? '_blank' : undefined}>
+                    <span>{text}</span>
+                    <img
+                      src={alt ? ArrowRight : ButtonArrowWhite}
+                      alt="arrow"
+                    />
+                  </RideToButton>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-        <div className={styles.sectionContent}>
-          <h3 className={styles.sectionName}>{name}</h3>
-          <h2 className={styles.sectionTitle}>{title}</h2>
-          {!!text && <p>{text}</p>}
-          {!!Object.keys(faqs).length && (
-            <CourseTypeDetails
-              courseType={{ details: faqs }}
-              minimal
-              spacedOut
-            />
-          )}
-          {!!links.length && (
-            <div className={styles.sectionButtons}>
-              {links.map(({ url, text, alt, external }, i) => (
-                <RideToButton
-                  className={styles.sectionButton}
-                  key={i}
-                  href={url}
-                  alt={alt}
-                  target={external ? '_blank' : undefined}>
-                  <span>{text}</span>
-                  <img src={alt ? ArrowRight : ButtonArrowWhite} alt="arrow" />
-                </RideToButton>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
+      </Waypoint>
     </div>
   )
 })
