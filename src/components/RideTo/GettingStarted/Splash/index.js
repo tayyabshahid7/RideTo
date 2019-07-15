41a3b7f5d1c1a767cbae4e9a94fd932851eb1a4a
James Coventry <jamesc@rideto.com>
2019-07-15 11:00:18 +0100

    PRODEV-1384. Fix full screen height on mobile

--- src/components/RideTo/GettingStarted/Splash/index.js
+++ src/components/RideTo/GettingStarted/Splash/index.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import styles from './styles.scss'
 import WhiteArrowDown from 'assets/images/rideto/WhiteArrowDown.svg'
+import Div100vh from 'react-div-100vh'
+import { useMediaQuery } from 'react-responsive'
 
 const imageSplash =
   'https://bike-tests.s3.eu-west-2.amazonaws.com/static/images/getting-started-splash.jpg'
 
 function Splash({ scrollTo }) {
+  let height = 'calc(100rvh - 56px)'
+
+  if (useMediaQuery({ query: '(min-width: 801px)' })) {
+    height = 'calc(100rvh - 80px)'
+  }
+
+  if (useMediaQuery({ query: '(min-width: 933px' })) {
+    height = 'calc(100rvh - 80px - 89px)'
+  }
+
   const handleButtonClick = () => {
     scrollTo(0)
   }
 
   return (
-    <div
+    <Div100vh
       className={styles.splash}
-      style={{ backgroundImage: `url(${imageSplash})` }}>
+      style={{
+        backgroundImage: `url(${imageSplash})`,
+        height
+      }}>
       <h1 className={styles.splashTitle}>Learn to Ride</h1>
       <p className={styles.splashLead}>
         It's time to ride. Whether you're new to bikes or a regular rider,
@@ -27,7 +42,7 @@ function Splash({ scrollTo }) {
         <img src={WhiteArrowDown} alt="" />
         <img src={WhiteArrowDown} alt="" />
       </button>
-    </div>
+    </Div100vh>
   )
 }
 
