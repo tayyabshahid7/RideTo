86e6915a73a9038ca3a49ded9e7464d4adbe825c
James Coventry <jamesc@rideto.com>
2019-07-19 16:11:04 +0100

    PRODEV-1365. Add icons

--- src/components/RideTo/ResultPage/FullLicenceIncluded/content.js
+++ src/components/RideTo/ResultPage/FullLicenceIncluded/content.js
@@ -1,34 +1,41 @@
+import instructor from './icons/instructor.svg'
+import fuel from './icons/fuel.svg'
+import bike from './icons/bike.svg'
+import onlinepractice from './icons/onlinepractice.svg'
+import paylater from './icons/paylater.svg'
+import customersupport from './icons/customersupport.svg'
+
 const CONTENT = {
   title: "What's included in the full licence training course",
   items: [
     {
-      icon: 'https://via.placeholder.com/30x30',
+      icon: instructor,
       title: 'Trusted local instructor',
       text: 'Every instructor is chosen based on learner reviews and feedback'
     },
     {
-      icon: 'https://via.placeholder.com/30x30',
+      icon: fuel,
       title: ' Test fees and fuel',
       text: 'No hidden extras to pay, we’ll cover all your course expenses'
     },
     {
-      icon: 'https://via.placeholder.com/30x30',
+      icon: bike,
       title: ' Bike & equipment hire',
       text: 'Everything you need to learn to ride'
     },
     {
-      icon: 'https://via.placeholder.com/30x30',
+      icon: onlinepractice,
       title: 'Online practice',
       text: 'Access our library of guides and revision to get test ready '
     },
     {
-      icon: 'https://via.placeholder.com/30x30',
+      icon: paylater,
       title: 'Buy now, pay later',
       text:
         'All full licence courses are available on finance, pay just £25 to book'
     },
     {
-      icon: 'https://via.placeholder.com/30x30',
+      icon: customersupport,
       title: 'Awesome customer support',
       text: 'Our dedicated support team of riders is on hand to help you'
     }
