31b60a8beef38f08758b4d4d716b9accc5191edf
James Coventry <jamesc@rideto.com>
2019-07-31 10:37:38 +0100

    PRODEV-1417. Add faq text

--- src/components/RideTo/CheckoutPage/OrderSummary/faqs.js
+++ src/components/RideTo/CheckoutPage/OrderSummary/faqs.js
@@ -1,14 +1,26 @@
 const FAQS = {
-  "What's included?":
-    'Cupidatat reprehenderit voluptate esse aute nisi aliqua id do.',
-  'What do I need to bring?':
-    'Cupidatat reprehenderit voluptate esse aute nisi aliqua id do.',
+  "What's included?": `Bike hire: choice of automatic or manual
+
+Helmet
+
+Gloves and Jacket (where indicated)
+
+Insurance
+
+Online pre-training`,
+  'What do I need to bring?': `Have the correct licence card: UK driving, provisional or EU licence with UK counterpart licence number
+
+Wear suitable clothing including sturdy jeans, jacket and boots
+
+Have your National Insurance number for a licence check
+
+Lunch or money for lunch`,
   'What happens next?':
-    'Cupidatat reprehenderit voluptate esse aute nisi aliqua id do.',
+    'Your booking will be confirmed within the next 3 working hours and we’ll send a confirmation email and SMS with all the training details. You won’t be charged until your booking is confirmed ',
   'Is my payment secure?':
-    'Cupidatat reprehenderit voluptate esse aute nisi aliqua id do.',
+    'Yes, your payment is processed securely with bank grade security including end-to-end 256 bit secure encryption and a SSL server to protects your card information',
   'Can I pay in cash?':
-    'Cupidatat reprehenderit voluptate esse aute nisi aliqua id do.'
+    'Unfortunately you are unable to book with a cash payment, as all instructors are paid electronically'
 }
 
 export default FAQS
