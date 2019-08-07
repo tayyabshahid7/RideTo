f52bbd2b44268cfdf057dea884d9389a805cbd18
James Coventry <jamesc@rideto.com>
2019-08-07 14:06:49 +0100

    PRODEV-1512/ Create post booking button

--- src/components/RideTo/Account/DashboardPageV2/CourseFeedback/index.js
+++ src/components/RideTo/Account/DashboardPageV2/CourseFeedback/index.js
@@ -1,35 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.scss'
 import classnames from 'classnames'
 
+const OPTIONS = [
+  {
+    id: 'completed',
+    text: 'I completed the course!',
+    message:
+      'Congratulations! That\'s great news, onto the next step! <a href="/">Leave us a review on TrustPilot</a>',
+    icon: 'happy'
+  },
+  {
+    id: 'didntComplete',
+    text: "I didn't complete the course",
+    message:
+      'Sorry to hear that, email us at <a href="mailto:hello@rideto.com">hello@rideto.com</a> to organise further training.',
+    icon: 'sad'
+  },
+  {
+    id: 'unableToStart',
+    text: 'I was unable to start the course',
+    message:
+      'Sorry to hear that, email us at <a href="mailto:hello@rideto.com">hello@rideto.com</a> to rebook.',
+    icon: 'sad'
+  }
+]
+
 function CourseFeedback() {
+  const [selected, setSelected] = useState(null)
+  const selectedOption = OPTIONS.find(({ id }) => id === selected)
+
   return (
     <div>
       <h3 className={styles.subtitle}>
         Your CBT course was booked for today, how did it go?
       </h3>
       <div>
-        <button className={styles.button}>I completed the course!</button>
-        <button className={classnames(styles.button, styles.buttonActive)}>
-          I didn't complete the course
-        </button>
-        <button className={styles.button}>
-          I was unable to start the course
-        </button>
+        {OPTIONS.map(({ id, text }) => (
+          <button
+            key={id}
+            className={classnames(
+              styles.button,
+              selected === id && styles.buttonActive
+            )}
+            onClick={() => {
+              setSelected(id)
+            }}>
+            {text}
+          </button>
+        ))}
       </div>
-      <div className={styles.message}>
-        <img
-          className={styles.messageIcon}
-          src="https://via.placeholder.com/58x58"
-          alt="Icon"
-          width="58"
-          height="58"
-        />
-        <p>
-          Congratulations! That's great news, onto the next step!{' '}
-          <a href="/">Leave us a review on TrustPilot</a>
-        </p>
-      </div>
+      {selected && (
+        <div className={styles.message}>
+          <img
+            className={styles.messageIcon}
+            src="https://via.placeholder.com/58x58"
+            alt="Icon"
+            width="58"
+            height="58"
+          />
+          <p dangerouslySetInnerHTML={{ __html: selectedOption.message }} />
+        </div>
+      )}
     </div>
   )
 }
