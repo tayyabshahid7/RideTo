869e75422a98d3a833bac8a4c8d23ee91b790b13
James Coventry <jamesc@rideto.com>
2019-08-20 14:12:33 +0100

    PRODEV-1532. Update times

--- src/components/RideTo/Contact/index.js
+++ src/components/RideTo/Contact/index.js
@@ -48,7 +48,7 @@ function Contact() {
               <img src={chat} alt="Chat" />
               <div className={styles.keyInfoItemText}>
                 <h2>Chat with us</h2>
-                <p>Mon-Sat 9am - 6pm • Sun Closed</p>
+                <p>Mon-Sun 9am - 6pm</p>
               </div>
               <div className={styles.keyInfoItemTextMobile}>Chat</div>
             </div>
@@ -56,7 +56,7 @@ function Contact() {
               <img src={call} alt="Call" />
               <div className={styles.keyInfoItemText}>
                 <h2>020 3603 9652</h2>
-                <p>Mon-Sat 9am - 6pm • Sun Closed</p>
+                <p>Mon-Sun 9am - 6pm</p>
               </div>
               <div className={styles.keyInfoItemTextMobile}>Call</div>
             </a>
