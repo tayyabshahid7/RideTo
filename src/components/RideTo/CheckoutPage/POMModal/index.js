e597e7695eef98801b6dd2d4febef8bb210d7a47
James Coventry <jamesc@rideto.com>
2019-09-04 14:37:45 +0100

    PRODEV-1582. Add pom buttons

--- src/components/RideTo/CheckoutPage/POMModal/index.js
+++ src/components/RideTo/CheckoutPage/POMModal/index.js
@@ -18,10 +18,12 @@ function POMModal({ closeModal, handleAddPOM }) {
       <div className={styles.title}>Recommended for new riders</div>
       <POMSelector popup />
       <div className={styles.buttons}>
-        <RideToButton alt onClick={handleNoClick}>
+        <RideToButton alt onClick={handleNoClick} id="pom-popup-no">
           No thanks
         </RideToButton>{' '}
-        <RideToButton onClick={handleYesClick}>Yes please</RideToButton>
+        <RideToButton onClick={handleYesClick} id="pom-popup-yes">
+          Yes please
+        </RideToButton>
       </div>
     </div>
   )
