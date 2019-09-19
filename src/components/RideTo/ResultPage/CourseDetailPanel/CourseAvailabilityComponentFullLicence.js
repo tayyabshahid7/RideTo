7cf7502169deea4526997fc9ce3402d61db30132
James Coventry <jamesc@rideto.com>
2019-09-19 10:23:10 +0100

    PRODEV-1612. Fix spelling

--- src/components/RideTo/ResultPage/CourseDetailPanel/CourseAvailabilityComponentFullLicence.js
+++ src/components/RideTo/ResultPage/CourseDetailPanel/CourseAvailabilityComponentFullLicence.js
@@ -113,8 +113,8 @@ class CourseAvailabilityComponentFullLicence extends Component {
               )}>
               Customise the right package for your riding ambitions. The
               instructor will be in touch within 24 hours to book the training
-              and test dates which work for you. To discuss what course if for
-              you or paying on finance,{' '}
+              and test dates which work for you. To discuss what course is right
+              for you or paying on finance,{' '}
               <a href="/contact">please get in touch</a>.
             </p>
             <CourseTypeDetails
