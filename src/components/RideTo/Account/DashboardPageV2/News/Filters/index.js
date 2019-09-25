40aa7154f737c7b0786e1026a93a5e6533a314e5
James Coventry <jamesc@rideto.com>
2019-09-25 13:25:00 +0100

    PRODEV-1566. Change news filters on mobile styling

--- src/components/RideTo/Account/DashboardPageV2/News/Filters/index.js
+++ src/components/RideTo/Account/DashboardPageV2/News/Filters/index.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import styles from './styles.scss'
 import classnames from 'classnames'
+import { useMediaQuery } from 'react-responsive'
 
 function Filters({ selectedFilter, handleFilterClick, filters }) {
+  const isDesktop = useMediaQuery({ minWidth: 768 })
+
   return (
     <ul className={styles.list}>
-      {filters.map(filter => (
-        <li className={styles.listItem} key={filter}>
-          <button
-            className={classnames(
-              styles.filterButton,
-              filter === selectedFilter && styles.active
-            )}
-            onClick={() => {
-              handleFilterClick(filter)
-            }}>
-            {filter.replace('How Tos', "How To's")}
-          </button>
-        </li>
-      ))}
+      {filters
+        .filter(item => !(!isDesktop && ['Events', 'Fun'].includes(item)))
+        .map(filter => (
+          <li className={styles.listItem} key={filter}>
+            <button
+              className={classnames(
+                styles.filterButton,
+                filter === selectedFilter && styles.active
+              )}
+              onClick={() => {
+                handleFilterClick(filter)
+              }}>
+              {!isDesktop & (filter === 'Latest Blogs')
+                ? 'Blogs'
+                : filter.replace('How Tos', "How To's")}
+            </button>
+          </li>
+        ))}
     </ul>
   )
 }
