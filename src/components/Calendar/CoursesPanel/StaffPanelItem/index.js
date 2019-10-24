6af5abf4c5b42c05b5d35a7f7d06762112596f3a
James Coventry <jamesc@rideto.com>
2019-10-24 15:35:32 +0100

    PRODEV-1601. Add notes to events

--- src/components/Calendar/CoursesPanel/StaffPanelItem/index.js
+++ src/components/Calendar/CoursesPanel/StaffPanelItem/index.js
@@ -4,7 +4,7 @@ import styles from './StaffPanelItem.scss'
 import personIcon from '../../../../assets/images/person.png'
 
 const StaffPanelItem = ({ event, date, staffId }) => {
-  const { all_day } = event
+  const { all_day, notes } = event
   const isSelected = parseInt(staffId) === event.id
 
   return (
@@ -15,26 +15,29 @@ const StaffPanelItem = ({ event, date, staffId }) => {
           isSelected && styles.headingSelected
         )}>
         <div className={styles.title} style={{ background: event.colour }}>
-          <div className={styles.body}>
-            <img src={personIcon} alt="" className={styles.instructorIcon} />{' '}
-            <div className={styles.name}>{event.instructor_name}</div>
-            <span className={styles.pipe}>|</span>
-            <span className={styles.time}>
-              {all_day ? (
-                'All day'
-              ) : (
-                <React.Fragment>
-                  {event.start_time.substring(0, 5)} -{' '}
-                  {event.end_time.substring(0, 5)}
-                </React.Fragment>
-              )}
-            </span>
+          <div className={styles.top}>
+            <div className={styles.body}>
+              <img src={personIcon} alt="" className={styles.instructorIcon} />{' '}
+              <div className={styles.name}>{event.instructor_name}</div>
+              <span className={styles.pipe}>|</span>
+              <span className={styles.time}>
+                {all_day ? (
+                  'All day'
+                ) : (
+                  <React.Fragment>
+                    {event.start_time.substring(0, 5)} -{' '}
+                    {event.end_time.substring(0, 5)}
+                  </React.Fragment>
+                )}
+              </span>
+            </div>
+            <a
+              className={styles.editLink}
+              href={`/calendar/${date}/staff/${event.instructor}/${event.id}/edit`}>
+              Edit
+            </a>
           </div>
-          <a
-            className={styles.editLink}
-            href={`/calendar/${date}/staff/${event.instructor}/${event.id}/edit`}>
-            Edit
-          </a>
+          {notes && <div className={styles.notes}>{notes}</div>}
         </div>
       </div>
     </div>
