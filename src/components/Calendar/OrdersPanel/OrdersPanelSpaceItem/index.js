e994fc1333196b9873dae0300eb4f3ef401dc29e
James Coventry <jamesc@rideto.com>
2019-10-25 16:51:17 +0100

    PRODEV-1601. add isadmin

--- src/components/Calendar/OrdersPanel/OrdersPanelSpaceItem/index.js
+++ src/components/Calendar/OrdersPanel/OrdersPanelSpaceItem/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import styles from './style.scss'
 import { Button } from 'reactstrap'
+import { connect } from 'react-redux'
+import { isAdmin } from 'services/auth'
 
-const OrdersPanelSpaceItem = ({ onAdd, onRemove }) => {
+const OrdersPanelSpaceItem = ({ onAdd, onRemove, isAdmin }) => {
   return (
     <div className={styles.container}>
       <span className={styles.info}>Available</span>
-      <Button
-        color="primary"
-        onClick={onAdd}
-        className={`mr-1 btn-padding-md ${styles.button}`}
-        size="sm">
-        Add order
-      </Button>
+      {isAdmin && (
+        <Button
+          color="primary"
+          onClick={onAdd}
+          className={`mr-1 btn-padding-md ${styles.button}`}
+          size="sm">
+          Add order
+        </Button>
+      )}
     </div>
   )
 }
 
-export default OrdersPanelSpaceItem
+const mapStateToProps = (state, ownProps) => {
+  return {
+    isAdmin: isAdmin(state.auth.user)
+  }
+}
+
+export default connect(mapStateToProps)(OrdersPanelSpaceItem)
