8dcc5860d763e4dda801982f65eef625752919e4
James Coventry <jamesc@rideto.com>
2019-12-02 17:56:52 +0000

    PRODEV-1831. Change for 50cc

--- src/components/RideTo/ResultPage/CourseDetailPanel/BikePicker.js
+++ src/components/RideTo/ResultPage/CourseDetailPanel/BikePicker.js
@@ -115,8 +115,8 @@ const BikePicker = React.forwardRef(
                 }
                 disabled={isManualFull}>
                 {getMotorbikeLabel('MANUAL_50CC', isFullLicence)}{' '}
-                {parseInt(manual_bike_hire_cost, 10) > 0 &&
-                  ` (+£${manual_bike_hire_cost / 100})`}
+                {parseInt(bike_hire_cost, 10) > 0 &&
+                  ` (+£${bike_hire_cost / 100})`}
                 {isManualFull ? fullText : null}
               </button>
             )}
