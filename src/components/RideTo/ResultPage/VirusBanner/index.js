2b92f36fea3723911676575a63c79b4fe5e0abf7
Theodor Gieschen <gieschen.theo@googlemail.com>
2020-03-25 13:29:59 +0100

    PRODEV-20204. added tablet image

--- src/components/RideTo/ResultPage/VirusBanner/index.js
+++ src/components/RideTo/ResultPage/VirusBanner/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useMediaQuery } from 'react-responsive'
 const mobile =
+  'https://bike-tests.s3.eu-west-2.amazonaws.com/static/images/RideTo+Keyworker+Banner_600x300px-01.jpg'
+const tablet =
   'https://bike-tests.s3.eu-west-2.amazonaws.com/static/images/RideTo+Keyworker+Banner_1000x400px-03.jpg'
 const desktop =
   'https://bike-tests.s3.eu-west-2.amazonaws.com/static/images/RideTo+Keyworker+Banner_1036x312px-02.jpg'
@@ -14,12 +16,15 @@ const style = {
 
 function VirusBanner() {
   const isDesktop = useMediaQuery({ query: '(min-width: 769px)' })
+  const isMobile = useMediaQuery({
+    query: '(max-width: 425px)'
+  })
 
   return (
     <a href="https://www.rideto.com/blog/coronavirus-motorcycling-update">
       <img
         style={style}
-        src={isDesktop ? desktop : mobile}
+        src={isDesktop ? desktop : isMobile ? mobile : tablet}
         alt="Virus Update"
       />
     </a>
