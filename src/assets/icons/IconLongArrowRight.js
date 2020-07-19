498a37aa2119804f4b569a0c0a78c0d4f8383ee9
Fortunes <fortunes.teller@outlook.com>
2020-07-19 13:19:36 -0400

    Update home page, login page

---
+++ src/assets/icons/IconLongArrowRight.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const IconLongArrowRight = ({ className }) => (
+  <svg
+    width="32"
+    height="11"
+    viewBox="0 0 32 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M1 5.5H31"
+      stroke="#EDEDED"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M26.5 0.5L31.5 5.5L26.5 10.5"
+      stroke="#EDEDED"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+export default IconLongArrowRight
