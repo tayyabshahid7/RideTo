371a256b879b06b0f317463df2870a662e967928
Fortunes <fortunes.teller@outlook.com>
2020-10-29 15:32:52 +0000

    Fix default bike hires and deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,22 +23,6 @@ if [ ! -d "$BUILD_APP_DIR" ]; then
     exit 1
 fi
 
-# echo ""
-# echo "  Deploying..."
-# echo ""
-# echo "  Cloudfront ID: $AWS_CLOUDFRONT_ID"
-# echo "  Bucket: $AWS_S3_BUCKET"
-# echo ""
-
-# aws s3 sync ${BUILD_JS_DIR} s3://${AWS_S3_BUCKET}/client/js
-# aws s3 sync ${BUILD_CSS_DIR} s3://${AWS_S3_BUCKET}/client/css
-# CDN_PATHS="$CDN_PATHS /client/*"
-
-# if [ "${CDN_PATHS}" ]; then
-#     echo "Invalidate Cloudfront: $CDN_PATHS"
-#     aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_ID} --paths ${CDN_PATHS}
-# fi
-
 echo ""
 echo "  Deploying App..."
 echo ""
@@ -49,9 +33,5 @@ echo ""
 aws s3 sync ${BUILD_JS_DIR} s3://${AWS_S3_BUCKET}/client/js --delete
 aws s3 sync ${BUILD_CSS_DIR} s3://${AWS_S3_BUCKET}/client/css --delete
 aws s3 sync ${BUILD_APP_DIR} s3://${AWS_S3_APP_BUCKET}
-CDN_PATHS="$CDN_PATHS /*"
 
-if [ "${CDN_PATHS}" ]; then
-    echo "Invalidate Cloudfront: $CDN_PATHS"
-    aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_APP_ID} --paths ${CDN_PATHS}
-fi
+aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_APP_ID} --paths "/*"
