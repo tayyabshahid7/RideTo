f25cea9879e8776cf6b45dd3e62896865922aa13
Fortunes <fortunes.teller@outlook.com>
2021-01-20 04:19:21 -0500

    Update order date change to load available courses

--- src/components/ConnectForm/ConnectDatePicker.js
+++ src/components/ConnectForm/ConnectDatePicker.js
@@ -4,15 +4,37 @@ import classnames from 'classnames'
 import enGB from 'date-fns/locale/en-GB'
 import DatePicker, { registerLocale } from 'react-datepicker'
 import moment from 'moment'
+// import { range } from 'lodash'
+import { useMediaQuery } from 'react-responsive'
 
 registerLocale('en-GB', enGB)
 
+const MobileDateInput = ({ value, onClick }) => (
+  <div className={styles.customDateInput} onClick={onClick}>
+    {value}
+  </div>
+)
+
+// const years = range(1900, 2100)
+// const months = [
+//   'January',
+//   'February',
+//   'March',
+//   'April',
+//   'May',
+//   'June',
+//   'July',
+//   'August',
+//   'September',
+//   'October',
+//   'November',
+//   'December'
+// ]
+
 export default function ConnectDatePicker({
-  label,
-  type = 'text',
   popperPosition = 'bottom-start',
   onChange,
-  noWrapLabel,
+  onCalendarChange,
   value,
   id,
   name,
@@ -23,6 +45,8 @@ export default function ConnectDatePicker({
   iso,
   highlightDates
 }) {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+
   const handleChange = date => {
     onChange({
       target: {
@@ -31,6 +55,30 @@ export default function ConnectDatePicker({
         ...(iso && { value: moment(date).toISOString() })
       }
     })
+  }
+
+  // const handleInnerChanged = date => {
+  //   console.log('inner date', date)
+  // }
+
+  // const handleMonthChanged = month => {
+  //   console.log('inner month', month)
+  // }
+
+  // const handleYearChanged = year => {
+  //   console.log('inner year', year)
+  // }
+
+  // const getYear = date => {
+  //   return moment(date).year()
+  // }
+
+  // const getMonth = date => {
+  //   return moment(date).month()
+  // }
+
+  const handleCalendarChange = date => {
+    onCalendarChange && onCalendarChange(date)
   }
 
   let date = ''
@@ -57,7 +105,68 @@ export default function ConnectDatePicker({
         )}
         id={id || name}
         type="date"
+        // renderCustomHeader={({
+        //   date,
+        //   changeYear,
+        //   changeMonth,
+        //   decreaseMonth,
+        //   increaseMonth,
+        //   prevMonthButtonDisabled,
+        //   nextMonthButtonDisabled
+        // }) => (
+        //   <div
+        //     style={{
+        //       margin: 10,
+        //       display: 'flex',
+        //       justifyContent: 'center'
+        //     }}>
+        //     <button
+        //       onClick={event => {
+        //         handleInnerChanged(event)
+        //         decreaseMonth(event)
+        //       }}
+        //       disabled={prevMonthButtonDisabled}>
+        //       {'<'}
+        //     </button>
+        //     <select
+        //       value={getYear(date)}
+        //       onChange={({ target: { value } }) => {
+        //         handleYearChanged(value)
+        //         changeYear(value)
+        //       }}>
+        //       {years.map(option => (
+        //         <option key={option} value={option}>
+        //           {option}
+        //         </option>
+        //       ))}
+        //     </select>
+
+        //     <select
+        //       value={months[getMonth(date)]}
+        //       onChange={({ target: { value } }) => {
+        //         handleMonthChanged(months.indexOf(value))
+        //         changeMonth(months.indexOf(value))
+        //       }}>
+        //       {months.map(option => (
+        //         <option key={option} value={option}>
+        //           {option}
+        //         </option>
+        //       ))}
+        //     </select>
+
+        //     <button
+        //       onClick={event => {
+        //         handleInnerChanged(event)
+        //         increaseMonth(event)
+        //       }}
+        //       disabled={nextMonthButtonDisabled}>
+        //       {'>'}
+        //     </button>
+        //   </div>
+        // )}
         selected={date}
+        onMonthChange={handleCalendarChange}
+        onYearChange={handleCalendarChange}
         onChange={handleChange}
         dateFormat="dd/MM/yyyy"
         disabled={disabled}
@@ -69,6 +178,7 @@ export default function ConnectDatePicker({
         popperPlacement={popperPosition}
         maxDate={maxDate}
         highlightDates={highlightDates}
+        {...(isMobile && { customInput: <MobileDateInput /> })}
         locale="en-GB"
       />
     </React.Fragment>
