df778963c7a62bd90c36ce46e3126c85a86a7b37
Bruno Kanazawa <bruno@rideto.com>
2022-09-13 15:17:34 +0200

    PRODEV-2954 Open klarna blog in new page

--- src/components/RideTo/KlarnaBanner/index.js
+++ src/components/RideTo/KlarnaBanner/index.js
@@ -6,7 +6,10 @@ import styles from './styles.scss'
 export default function KlarnaBanner() {
   const isMobile = useMediaQuery({ maxWidth: 767 })
   return (
-    <a href="https://www.rideto.com/blog/motorcycle-cbt-on-finance">
+    <a
+      href="https://www.rideto.com/blog/motorcycle-cbt-on-finance"
+      target="_blank"
+      rel="noopener noreferrer">
       <div className={styles.container}>
         {!isMobile ? (
           <span>Split your purchase into 3 interest-free payments*</span>
