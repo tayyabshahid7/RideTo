9e5bdb735281b578ad74ef1c0b0abb12365fe66c
Bruno Kanazawa <bruno@rideto.com>
2022-10-12 18:37:40 +0200

    PRODEV-2942 regex phone UK

--- src/components/RideTo/CheckoutPage/UserDetails/index.js
+++ src/components/RideTo/CheckoutPage/UserDetails/index.js
@@ -117,8 +117,12 @@ class UserDetails extends Component {
 
   validatePhone(e) {
     const { handleErrors, errors, getErrorDivId } = this.props
-
-    if (!e.target.value.match(/^\+44\d{10}$/)) {
+    console.log(e.target.value)
+    if (
+      !e.target.value.match(
+        /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/
+      )
+    ) {
       errors['phone'] = 'Invalid phone number'
       if (!errors.divId) errors.divId = getErrorDivId('phone')
       handleErrors({ ...errors })
