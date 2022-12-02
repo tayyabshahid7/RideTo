92a2794c19610844310e23f6fae9a20a9948ab58
Bruno Kanazawa <bruno@rideto.com>
2022-12-02 12:11:32 +0100

    PRODEV-3029 Fixed map component pin click

--- src/components/RideTo/MapComponent/NewIconMapPin.jsx
+++ src/components/RideTo/MapComponent/NewIconMapPin.jsx
@@ -3,18 +3,21 @@ import React from 'react'
 import styles from './NewIconMapPin.scss'
 
 const NewIconMapPin = props => {
-  const { pricing, isInstantBooking = false } = props
-  return (
-    <>
-      <div className={styles.container}>
-        <div>
-          {isInstantBooking && <img src={InstantBookingIcon} alt="" />}
-          <span className={styles.pinPrice}>{pricing}</span>
-        </div>
+  const { pricing, isInstantBooking = false, handlePinClick, course } = props
 
-        <div className={styles.triangleTopLeft} />
+  return (
+    <div
+      className={styles.container}
+      onClick={() => {
+        handlePinClick(course, 3)
+      }}>
+      <div>
+        {isInstantBooking && <img src={InstantBookingIcon} alt="" />}
+        <span className={styles.pinPrice}>{pricing}</span>
       </div>
-    </>
+
+      <div className={styles.triangleTopLeft} />
+    </div>
   )
 }
 export default NewIconMapPin
