fe4a57dfb005abd229529993a496f42efd0f49ec
Bruno Kanazawa <bruno@rideto.com>
2022-12-15 10:01:47 +0000

    PRODEV-3030 WIP

--- src/components/RideTo/ResultPage/ResultsHeaderMobile/index.js
+++ src/components/RideTo/ResultPage/ResultsHeaderMobile/index.js
@@ -1,24 +1,43 @@
-import { IconCalendarMobile } from 'assets/icons'
+import { IconArrowDown, IconCalendarMobile, IconSearch } from 'assets/icons'
+import classNames from 'classnames'
 import React from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
 
 import styles from './styles.scss'
 
 function ResultsHeaderMobile(props) {
   const courseTitle = 'CBT training'
-  const postcode = 'CR6 9EE'
+  const postcode = 'London'
+  const miles = `+ ${0} miles`
 
-  const date = 'Choose a date'
+  const date = 'Choose a Date'
+
+  const handleSearchClick = e => {
+    console.log(e)
+  }
+
+  const handleChooseADateClick = e => {
+    console.log(e)
+  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <AiOutlineSearch className={styles.searchIcon} />
+      <div
+        className={classNames(styles.wrapper, styles.searchWrapper)}
+        onClick={handleSearchClick}>
+        <IconSearch className={classNames(styles.searchIcon, styles.icon)} />
         <strong>{courseTitle}</strong> in <strong>{postcode}</strong>
+        <div className={styles.miles}>
+          <span>{miles}</span>
+          <IconArrowDown className={styles.dropDownIcon} />
+        </div>
       </div>
-      <div className={styles.wrapper}>
-        <IconCalendarMobile />
-        <div>{date}</div>
+      <div className={styles.divider} />
+      <div
+        className={classNames(styles.wrapper, styles.wrapperChooseADate)}
+        onClick={handleChooseADateClick}>
+        <IconCalendarMobile className={styles.icon} />
+        <div className={styles.date}>{date}</div>
+        <IconArrowDown className={styles.dropDownIcon} />
       </div>
     </div>
   )
